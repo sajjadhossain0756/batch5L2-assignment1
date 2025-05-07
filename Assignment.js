64335eb4a3c8ccb6fe19e2d68e6f321353db039a
sajjadhossain <sajjadhossain0756@gmail.com>
2025-05-07 23:31:20 +0600

    concatenateArrays Problem is solve

--- Assignment.js
+++ Assignment.js
@@ -20,5 +20,13 @@ const books = [
     { title: "Book C", rating: 3.0 }
 ];
 const res4 = filterByRating(books);
-console.log(res4);
+// console.log(res4);
 // Output: [ { title: "Book A", rating: 4.5 }, { title: "Book C", rating: 5.0 } ]
+function concatenateArrays(...arrays) {
+    const [a, ...z] = [...arrays];
+    const concatArray = a.concat(...z);
+    return concatArray;
+}
+const res5 = concatenateArrays(["a", "b"], ["c"]);
+const res6 = concatenateArrays([1, 2], [3, 4], [5]);
+console.log(res5, res6);
