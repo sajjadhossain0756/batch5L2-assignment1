eb4bbb2faed75644a2619f0c29f46cf460b5c704
sajjadhossain <sajjadhossain0756@gmail.com>
2025-05-07 22:09:38 +0600

    filterByRating problem is solve

--- Assignment.js
+++ Assignment.js
@@ -10,4 +10,15 @@ function formatString(input, toUpper) {
 const res = formatString('Hello');
 const res1 = formatString('Hello', true);
 const res2 = formatString('Hello', false);
-console.log(res, res1, res2);
+function filterByRating(items) {
+    const result = items.filter(item => item.rating >= 4);
+    return result;
+}
+const books = [
+    { title: "Book A", rating: 4.5 },
+    { title: "Book B", rating: 3.2 },
+    { title: "Book C", rating: 3.0 }
+];
+const res4 = filterByRating(books);
+console.log(res4);
+// Output: [ { title: "Book A", rating: 4.5 }, { title: "Book C", rating: 5.0 } ]
