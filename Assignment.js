11e8bf9c54d90d326794cc192edd057f7cb01ba3
sajjadhossain <sajjadhossain0756@gmail.com>
2025-05-08 09:17:35 +0600

    processValue and getMostExpensiveProduct Problems are solve

--- Assignment.js
+++ Assignment.js
@@ -49,5 +49,31 @@ class Car extends Vehicle {
     }
 }
 const myCar = new Car("Toyota", 2020, "Corolla");
-console.log(myCar.getInfo());
-console.log(myCar.getModel());
+// console.log(myCar.getInfo());
+// console.log(myCar.getModel())
+function processValue(value) {
+    if (typeof value === "string") {
+        return value.length;
+    }
+    else {
+        return value * 2;
+    }
+}
+const res7 = processValue('hello');
+const res8 = processValue(10);
+function getMostExpensiveProduct(products) {
+    const expensiveProduct = products.sort((a, b) => b.price - a.price);
+    const res9 = expensiveProduct[0];
+    if (res9) {
+        return res9;
+    }
+    return null;
+}
+const products = [
+    { name: "Pen", price: 70 },
+    { name: "Notebook", price: 25 },
+    { name: "Bag", price: 50 }
+];
+const res10 = getMostExpensiveProduct(products);
+console.log(res10);
+// Output: { name: "Bag", price: 50 }
